f54ecba77679bbb07b27cfc7144b2fcd0ac4468a
Erlend Syljuåsen <esylju@gmail.com>
2021-12-25 23:03:43 +0100

    reduce frame size

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from camHandler import CamHandler
 
 if __name__ == "__main__":
-    handler = CamHandler(60, 400, 400)
+    handler = CamHandler(60, 200, 200)
     handler.calibrateCenter()
     handler.calibrateRadius()
     handler.mainLoop()
